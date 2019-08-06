2e4718907ca9652f98a7df3118077262f469c3ff
Vitor Rios <vitor.neto@tron.com.br>
2019-08-06 18:00:07 -0300

    Ajuste no DevController adicionando create new DEV

--- backend/src/Controllers/DevController.js
+++ backend/src/Controllers/DevController.js
@@ -1,10 +1,38 @@
+const axios = require('axios');
+
+const Dev = require('../models/Dev')
+
+
 module.exports = {
 
     index(req, res) {
         return res.json({ ok: true })
     },
 
-    store(req, res) {
-        return res.json(req.body)
+    async store(req, res) {
+
+        const { userName } = req.body;
+
+        const userExists = await Dev.findOne({ user: userName });
+
+        if (userExists)
+            return res.json(userExists);
+
+        try {
+            const { data } = await axios.get(`https://api.github.com/users/${userName}`)
+
+            const { name, bio, avatar_url: avatar } = data;
+
+            const dev = await Dev.create({
+                name,
+                user: userName,
+                bio,
+                avatar,
+            });
+
+            return res.json(dev);
+        } catch (error) {
+            return res.json({ erro: 'O usuario informado não foi encontrado' })
+        }
     }
 }
